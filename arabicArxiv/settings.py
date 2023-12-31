b440874c634e1a8c2f6b4b6f8be0e1619f516e60
Hadjer-Benmeziane <hadjer.benmeziane@ibm.com>
2023-12-31 10:55:05 +0100

    Automatic configuration with django-heroku

--- arabicArxiv/settings.py
+++ arabicArxiv/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os 
+import django_on_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+django_on_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
